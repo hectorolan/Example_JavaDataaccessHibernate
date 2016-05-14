19d98539e2ecfcd5702b8ff0f85bd527b7fe10de
olanhect <olanhector@gmail.com>
2016-05-14 19:56:54 -0400

    Implement MenuItem Dao

--- pam-dataaccess/src/main/java/com/olanh/pam_dataaccess/hibernate/DAOMenuItem.java
+++ pam-dataaccess/src/main/java/com/olanh/pam_dataaccess/hibernate/DAOMenuItem.java
@@ -10,116 +10,164 @@ import org.hibernate.Transaction;
 
 import com.olanh.olanh_entities.MenuItem;
 import com.olanh.olanh_entities.Place;
+import com.olanh.olanh_entities.list.MenuItems;
+import com.olanh.olanh_entities.list.Places;
+import com.olanh.olanh_entities.status.StatusAdd;
+import com.olanh.olanh_entities.status.StatusDelete;
+import com.olanh.olanh_entities.status.StatusGet;
+import com.olanh.olanh_entities.status.StatusUpdate;
+import com.olanh.pam_dataaccess.util.DAOResponseUtil;
 import com.olanh.pam_dataaccess.util.HibernateUtil;
 
 public class DAOMenuItem {
-	/*	int Id;int PlaceId;String Name;
-	 *  List<String> Content;double Price;String Image;
-	 *  String Category;String Remark;String Calories;
-	*/
-	
-	
-	//CREATE
-	public Integer addMenuItem(MenuItem menuItem) {
-		Transaction tx = null;
-		Session session = HibernateUtil.getSession();
-		Integer menuItemID = null;
+	/*
+	 * int Id;int PlaceId;String Name; List<String> Content;double Price;String
+	 * Image; String Category;String Remark;String Calories;
+	 */
+
+	// CREATE
+	public DAOResponseUtil<StatusAdd, MenuItem> addMenuItem(MenuItem menuItem) {
+		DAOResponseUtil<StatusAdd, MenuItem> response = new DAOResponseUtil<StatusAdd, MenuItem>();
+		Long menuItemID = null;
 		try {
-			tx = session.beginTransaction();
-
-			menuItemID = (Integer) session.save(menuItem);
-			session.flush();
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
-			e.printStackTrace();
-		} finally {
-			session.close();
-		}
-		return menuItemID;
-	}
-
-	//READ
-	public ArrayList<MenuItem> allMenuItems() {
-		ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
-
-		Session session = HibernateUtil.getSession();
-		Transaction tx = null;
-		try {
-			tx = session.beginTransaction();
-			List<?> sourceMenuItems = session.createQuery("FROM MENUITEM").list();
-			for (Iterator<?> iterator = sourceMenuItems.iterator(); iterator.hasNext();) {
-				MenuItem menuItem = (MenuItem) iterator.next();
-				menuItems.add(menuItem);
+			Transaction tx = null;
+			Session session = HibernateUtil.getSession();
+			try {
+				tx = session.beginTransaction();
+				menuItemID = (Long) session.save(menuItem);
+				if (menuItemID == null)
+					response.setStatus(StatusAdd.ALREADY_EXISTS);
+				else {
+					menuItem.setId(menuItemID);
+					response.setStatus(StatusAdd.OK);
+				}
+				session.flush();
+				tx.commit();
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				response.setStatus(StatusAdd.DB_ERROR);
+				if (tx != null)
+					tx.rollback();
+			} finally {
+				session.close();
 			}
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
+		} catch (Throwable e) {
 			e.printStackTrace();
-		} finally {
-			session.close();
+			response.setStatus(StatusAdd.DB_ERROR);
 		}
-
-		return menuItems;
+		if (menuItem.getId() == null) {
+			menuItem.setId((long) 2);
+		}
+		return response.setResponse(menuItem);
 	}
 
-	//UPDATE
-	public MenuItem updateMenuItem(MenuItem menuItem) {
-		Session session = HibernateUtil.getSession();
-		Transaction tx = null;
+	// READ
+	public DAOResponseUtil<StatusGet, MenuItems> allMenuItems(long placeId) {
+		DAOResponseUtil<StatusGet, MenuItems> response = new DAOResponseUtil<StatusGet, MenuItems>();
+		MenuItems menuItems = new MenuItems();
+		try {
+			Session session = HibernateUtil.getSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				List<?> sourceMenuItems = session.createQuery("FROM MENUITEM").list();
+				for (Iterator<?> iterator = sourceMenuItems.iterator(); iterator.hasNext();) {
+					MenuItem menuItem = (MenuItem) iterator.next();
+					menuItems.add(menuItem);
+				}
+				tx.commit();
+				response.setStatus(StatusGet.OK);
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				response.setStatus(StatusGet.DB_ERROR);
+				if (tx != null)
+					tx.rollback();
+			} finally {
+				session.close();
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+			response.setStatus(StatusGet.DB_ERROR);
+			menuItems = this.offlineDBExampleList();
+		}
+		// This if is temporal while testing/developing
+		if (menuItems.size() == 0)
+			menuItems = this.offlineDBExampleList();
+		return response.setResponse(menuItems);
+	}
+
+	// UPDATE
+	public DAOResponseUtil<StatusUpdate, MenuItem> updateMenuItem(MenuItem menuItem) {
+		DAOResponseUtil<StatusUpdate, MenuItem> response = new DAOResponseUtil<StatusUpdate, MenuItem>();
 		MenuItem menuItemFromDB = null;
 		try {
-			tx = session.beginTransaction();
-			menuItemFromDB = (MenuItem) session.get(MenuItem.class, menuItem.getId());
-			menuItemFromDB.setCalories(menuItem.getCalories());
-			menuItemFromDB.setCategory(menuItem.getCategory());
-			menuItemFromDB.setContent(menuItem.getContent());
-			menuItemFromDB.setId(menuItem.getId());
-			menuItemFromDB.setImage(menuItem.getImage());
-			menuItemFromDB.setName(menuItem.getName());
-			menuItemFromDB.setPlaceId(menuItem.getPlaceId());
-			menuItemFromDB.setPrice(menuItem.getPrice());
-			menuItemFromDB.setRemark(menuItem.getRemark());
-			//menuItemFromDB.(menuItem);
-			
-			session.update(menuItem);
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
+			Session session = HibernateUtil.getSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				menuItemFromDB = (MenuItem) session.get(MenuItem.class, menuItem.getId());
+				menuItemFromDB.setCalories(menuItem.getCalories());
+				menuItemFromDB.setCategory(menuItem.getCategory());
+				menuItemFromDB.setContent(menuItem.getContent().toString());
+				menuItemFromDB.setId(menuItem.getId());
+				menuItemFromDB.setImage(menuItem.getImage());
+				menuItemFromDB.setName(menuItem.getName());
+				menuItemFromDB.setPlaceId(menuItem.getPlaceId());
+				menuItemFromDB.setPrice(menuItem.getPrice());
+				menuItemFromDB.setRemark(menuItem.getRemark());
+				// menuItemFromDB.(menuItem);
+				session.update(menuItem);
+				tx.commit();
+				response.setStatus(StatusUpdate.UPDATED);
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				response.setStatus(StatusUpdate.DB_ERROR);
+				if (tx != null)
+					tx.rollback();
+			} finally {
+				session.close();
+			}
+		} catch (Throwable e) {
 			e.printStackTrace();
-		} finally {
-			session.close();
+			response.setStatus(StatusUpdate.DB_ERROR);
 		}
-		
-		return menuItemFromDB;
+		if (menuItemFromDB == null) {
+			menuItemFromDB = this.offlineDBExampleObject();
+		}
+		return response.setResponse(menuItemFromDB);
 	}
 
-	//DELETE
-	public boolean deleteMenuItem(long menuItemID) {
-		Session session = HibernateUtil.getSession();
-		Transaction tx = null;
+	// DELETE
+	public DAOResponseUtil<StatusDelete, MenuItem> deleteMenuItem(long menuItemID) {
+		DAOResponseUtil<StatusDelete, MenuItem> response = new DAOResponseUtil<StatusDelete, MenuItem>();
 		try {
-			tx = session.beginTransaction();
-			MenuItem menuItem = (MenuItem) session.get(MenuItem.class, menuItemID);
-			session.delete(menuItem);
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
+			Session session = HibernateUtil.getSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				MenuItem menuItem = (MenuItem) session.get(MenuItem.class, menuItemID);
+				session.delete(menuItem);
+				tx.commit();
+				response.setStatus(StatusDelete.DELETED);
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				session.close();
+				response.setStatus(StatusDelete.DB_ERROR);
+				if (tx != null)
+					tx.rollback();
+			} finally {
+				session.close();
+			}
+		} catch (Throwable e) {
 			e.printStackTrace();
-			session.close();
-			return false;
-		} finally {
-			session.close();
+			response.setStatus(StatusDelete.DB_ERROR);
 		}
-		return true;
+		return response;
 	}
 
-	//GET
-	public MenuItem getMenuItem(long menuItemId){
+	// GET
+	// not implemented
+	public MenuItem getMenuItem(long menuItemId) {
 		Session session = HibernateUtil.getSession();
 		MenuItem menuItem = null;
 		try {
@@ -129,9 +177,43 @@ public class DAOMenuItem {
 		} finally {
 			session.close();
 		}
-		
+
 		return menuItem;
 	}
 
+	// For TEST and Offline uses
+	private MenuItems offlineDBExampleList() {
+		MenuItems menuItems = new MenuItems();
+		for (int i = 0; i <= 5; i++){
+			MenuItem menuItem = offlineDBExampleObject();
+			menuItem.setContent("Tomates,Beans,Cheese,Guacamole"+i+"'");
+			menuItem.addIngrediet("Tomatoe");
+			menuItem.addIngrediet("Beans");
+			menuItem.addIngrediet("Cheese");
+			menuItem.addIngrediet("Guacamole");
+			menuItem.setId(i+1);
+			menuItems.add(menuItem);
+			if (i == 5){
+				menuItem.setCategory("Main");
+			}
+		}
+		return menuItems;
+	}
+
+	private MenuItem offlineDBExampleObject() {
+		MenuItem menuItem = new MenuItem();
+		menuItem.setId(1);
+		menuItem.setAvailable(true);
+		menuItem.setCalories("125");
+		menuItem.setContent("['Tomates','Beans','Cheese','Guacamole']");
+		menuItem.setId(1);
+		menuItem.setImage("");
+		menuItem.setName("La Comidita2");
+		menuItem.setPlaceId(1);
+		menuItem.setPrice(4.98);
+		menuItem.setRemark("The chef top pics");
+		menuItem.setCategory("Burger");
+		return menuItem;
+	}
 
 }
